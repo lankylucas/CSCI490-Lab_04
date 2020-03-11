6586d8695e6847ce33c57570d33f6fe91dc60b8f
Anastasiya Romanova <romanovaaa@g.cofc.edu>
2020-03-11 17:03:02 -0400

    webview lab

---
+++ app/src/main/java/com/example/csci490_lab_04/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.csci490_lab_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("https://berniesanders.com");
+    }
+}
