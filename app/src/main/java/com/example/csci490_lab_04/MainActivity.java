5b5de2ca0e9bf651b8f2d725e9311c2c18d92ed8
Anastasiya Romanova <romanovaaa@g.cofc.edu>
2020-03-11 21:15:34 -0400

    url connection lab

--- app/src/main/java/com/example/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/csci490_lab_04/MainActivity.java
@@ -3,7 +3,20 @@ package com.example.csci490_lab_04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+
+import static android.util.Log.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
         WebView webview = new WebView(this);
         setContentView(webview);
         webview.loadUrl("https://berniesanders.com");
+    }
+    public static String UrlConnection(String urlString) throws IOException {
+        String url = "https://jsonplaceholder.typicode.com/users";
+        URL myURL = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) myURL.openConnection();
+        InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+        BufferedReader in = new BufferedReader(isr);
+        String line = in.readLine();
+        in.close();
+      Log.i("whatever",line);
+       return  line;
+
+
+
+
     }
 }
